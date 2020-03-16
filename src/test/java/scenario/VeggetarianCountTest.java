975ec4b4685299edf7791c844f3a1f09aedd162d
vdsalexandre <alexandre.vandensteen@gmail.com>
2020-03-16 10:10:39 +0100

    initial commit

---
+++ src/test/java/scenario/VeggetarianCountTest.java
@@ -0,0 +1,13 @@
+package scenario;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VeggetarianCountTest {
+
+    @Test
+    public void testEnv() {
+        assertThat(true).isTrue();
+    }
+}
