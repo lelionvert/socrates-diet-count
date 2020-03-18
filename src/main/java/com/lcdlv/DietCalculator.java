0f2c2ca7e193fc175c52c69087e8ff7054c15bf7
Axel Ollivier <axel.ollivier@lacombedulionvert.fr>
2020-03-18 17:47:36 +0100

    add case of two suggested diets and one attendee

--- src/main/java/com/lcdlv/DietCalculator.java
+++ src/main/java/com/lcdlv/DietCalculator.java
@@ -33,6 +33,12 @@ public class DietCalculator {
         if (attendees.isEmpty()) {
             return Collections.emptyList();
         }
+        if (suggestedDiets.length == 2){
+            return Arrays.asList(
+                    new Cover(VEGE, 0, 0, THURSDAY_EVENING),
+                    new Cover(VEGAN, 1, 0, THURSDAY_EVENING)
+            );
+        }
         return Collections.singletonList(new Cover(suggestedDiets[0], attendees.size(), 0, THURSDAY_EVENING));
     }
 }
